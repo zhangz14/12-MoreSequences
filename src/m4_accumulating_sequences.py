6522e058d13c06282778913bb3984c2e94b63ce8
zhangz14 <zhangz14@rose-hulman.edu>
2018-04-01 16:59:52 -0400

    12

--- src/m4_accumulating_sequences.py
+++ src/m4_accumulating_sequences.py
@@ -127,17 +127,19 @@ def make_simple_string(m, n):
       :type n: int
     """
     # ------------------------------------------------------------------
-    # TODO: 5. Implement and test this function.
+    # DONE: 5. Implement and test this function.
     #   Note that you should write its TEST function first (above).
     # ------------------------------------------------------------------
-    for k in range(m, n + 1):
-
+    sequence = ''
+    for k in range(m, n + 1, 1):
+        sequence = sequence + str(k) + '-'
+    return sequence
 
 
 def run_test_make_less_simple_string():
     """ Tests the   make_less_simple_string    function. """
     # ------------------------------------------------------------------
-    # TODO: 6. Implement this TEST function.
+    # DONE: 6. Implement this TEST function.
     #   It TESTS the  make_less_simple_string  function defined below.
     #   Include at least **   2   ** tests.
     #
@@ -147,6 +149,16 @@ def run_test_make_less_simple_string():
     print('--------------------------------------------------')
     print('Testing the   make_less_simple_string   function:')
     print('--------------------------------------------------')
+
+    expected = '5-6-7-8-9-10-11-12-13'
+    actual = make_less_simple_string(5, 13)
+    print('Expected:', expected)
+    print('Actual:  ', actual)
+
+    expected = '205'
+    actual = make_less_simple_string(205, 205)
+    print('Expected:', expected)
+    print('Actual:  ', actual)
 
 
 def make_less_simple_string(m, n):
@@ -172,9 +184,16 @@ def make_less_simple_string(m, n):
       :type n: int
     """
     # ------------------------------------------------------------------
-    # TODO: 7. Implement and test this function.
+    # DONE: 7. Implement and test this function.
     #   Note that you should write its TEST function first (above).
     # -----------------------------------------------------------------
+    sequence = ''
+    for k in range(m, n + 1, 1):
+        if k < n:
+            sequence = sequence + str(k) + '-'
+        elif k == n:
+            sequence = sequence + str(k)
+    return sequence
 
 
 def run_test_draw_shapes():
